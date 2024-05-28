1f05748bb4fc0ce84cf278ac9fcd6bdbfab692fd
Bilal0410 <bilaln0410@gmail.com>
2024-05-28 20:58:00 +0200

    styling update venue button

--- src/components/UpdateVenueForm/index.jsx
+++ src/components/UpdateVenueForm/index.jsx
@@ -358,7 +358,7 @@ export default function UpdateVenueForm({ venue, onClose, onUpdate }) {
             </button>
             <button
               type="submit"
-              className="ml-4 inline-flex justify-center rounded-md border border-transparent bg-blue-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="ml-4 inline-flex justify-center rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2"
             >
               Update Venue
             </button>
